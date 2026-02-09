b9f0436cbc9b2e4d27a448215081cbf651f770bd
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2026-02-09 17:10:33 -0500

    this is an experiment and starting out to see what gets changed or not

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Jorge's Blog",
+	url: "https://jorges-blog.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Jorge Auquilla",
+		email: "jorge.auquilla99@myhunter.cuny.edu",
+		url: "https://jorges-blog.com/about-me/"
 	}
 }
--- content/about.md
+++ content/about.md
@@ -4,6 +4,11 @@ const eleventyNavigation = {
 	order: 3
 };
 ---
-# About
 
-I am a person that writes stuff.
+# About Me
+
+Welcome to my Blog!
+
+I am an aspiring UI / UX designer and this is the start to where I talk about almost anything and everything that comes to my mind. I hope you can accompany me as I enhance my HTML, CSS & JS skills and showcase them through the use of styling this website.
+
+-- Jorge Auquilla
